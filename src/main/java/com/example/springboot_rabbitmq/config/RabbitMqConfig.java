b77ee13cefa6d574254958e7295bdf7a2ce95ef8
abhishekpotbhare01 <abhishek.potbhare01@gmail.com>
2023-08-11 16:14:12 +0530

    rabbitmq config

---
+++ src/main/java/com/example/springboot_rabbitmq/config/RabbitMqConfig.java
@@ -0,0 +1,40 @@
+package com.example.springboot_rabbitmq.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    @Value("rabbitmq.queue.name")
+    private String queueName;
+
+    @Value("rabbitmq.exchange.name")
+    private String exchangeName;
+    @Value("rabbitmq.routingKey")
+    private String routingKey;
+
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(exchangeName);
+    }
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(queue())
+                .to(exchange())
+                .with(routingKey);
+    }
+
+}
